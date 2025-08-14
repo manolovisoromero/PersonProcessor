56c9beefb93f1e68974924a73c24c2061e218cdf
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2025-08-14 18:18:20 +0200

    Initial project creation

---
+++ src/test/java/manolovisoromero/person_processor/PersonProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package manolovisoromero.person_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
