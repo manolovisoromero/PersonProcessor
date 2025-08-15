83a5746d93682ef61711127435ee356b3bd7148a
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2025-08-15 09:23:23 +0200

    - Added logging to the REST controller + updated the return to something more explicit

--- src/main/java/manolovisoromero/person_processor/controller/PersonController.java
+++ src/main/java/manolovisoromero/person_processor/controller/PersonController.java
@@ -1,8 +1,11 @@
 package manolovisoromero.person_processor.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import manolovisoromero.person_processor.dto.PersonDto;
 import manolovisoromero.person_processor.service.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +18,14 @@ public class PersonController {
     public static final String MATCH_THE_CRITERIA = "Currently 1 or more stored people match the criteria.";
     public static final String NO_ONE_MATCHES_YET = "No one matches yet.";
     private final PersonService personService;
-
+    private static final Logger log = LoggerFactory.getLogger(PersonController.class);
 
     @PostMapping
-    public ResponseEntity<String> upsertPerson(@RequestBody PersonDto personDto) {
+    public ResponseEntity<String> upsertPerson(@Valid @RequestBody PersonDto personDto) {
+        log.info("Received upsert request for person with id: {}", personDto.getId());
         var result = personService.processPerson(personDto);
         if(result.satisfied()){
-            return ResponseEntity.status(HttpStatus.valueOf(200)).body(MATCH_THE_CRITERIA);
+            return ResponseEntity.ok().body(MATCH_THE_CRITERIA);
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(NO_ONE_MATCHES_YET);
     }
