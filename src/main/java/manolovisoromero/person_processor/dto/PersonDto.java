2c0b791fb207971b72cba0774283b5ebdbba8648
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2025-08-15 09:23:56 +0200

    - Added @NotNull to the field which I except can't be null

--- src/main/java/manolovisoromero/person_processor/dto/PersonDto.java
+++ src/main/java/manolovisoromero/person_processor/dto/PersonDto.java
@@ -2,6 +2,7 @@ package manolovisoromero.person_processor.dto;
 
 
 import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 @Builder
 public class PersonDto implements Dto {
 
+    @NotNull
     private Long id;
     private String name;
     private Set<Long> parentIds;
