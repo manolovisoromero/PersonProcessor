5f1e4257bdbf5e11d9eb8ce642c03bb1760214b4
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2025-08-17 18:48:26 +0200

    - Updated Person to simultaneously be the domain model and the entity model.

--- src/main/java/manolovisoromero/person_processor/model/Person.java
+++ src/main/java/manolovisoromero/person_processor/model/Person.java
@@ -1,8 +1,12 @@
 package manolovisoromero.person_processor.model;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Data;
+import lombok.With;
 import manolovisoromero.person_processor.validation.NotSelfReferencing;
 
 import java.time.LocalDate;
@@ -10,17 +14,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 @NotSelfReferencing
-@EqualsAndHashCode(callSuper = true)
 @Builder(toBuilder = true)
-@Value
-public class Person extends Entity{
+@Data
+@With
+@Entity
+@Table(name = "person")
+public class Person {
 
+    @Id
     Long id;
+
     String name;
+
     LocalDate dateOfBirth;
+
     @Builder.Default
+    @ElementCollection
     Set<Long> parentIds = new HashSet<>();
+
     Long partnerId;
+
     @Builder.Default
+    @ElementCollection
     Set<Long> childrenIds = new HashSet<>();
-    }
+}
